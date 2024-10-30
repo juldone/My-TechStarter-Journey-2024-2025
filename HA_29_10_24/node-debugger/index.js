098899c0a41251731d27b799a84aa82a4a25b0c6
juliandete <julian@determeyser.info>
2024-10-30 12:04:06 +0100

    hallo

--- HA_29_10_24/node-debugger/index.js
+++ HA_29_10_24/node-debugger/index.js
@@ -1,21 +1,19 @@
 // index.js
-var items = [1, "2", 3, "four", 5, null, undefined, 6];
-const results = [];
+var items = [1, "2", 3, "four", 5, null, undefined, 6]
+const results = []
 
-items.forEach((item) => {  // foreach zu forEach ändern
-    if (typeof item === 'number') {
-        if (item % 2 === 0) {
-            results.pushHHHHH(item * 2);
-        }
-    } else if (item === null) {
-        console.logSSSSS("Found a null item");
-    } else {
-        // 'result' zu 'results' korrigieren
-        results.push(parseInt(item));
+items.foreach((item) => {
+    if(typeof item === 'number')
+        if(item % 2 === 0) results.push(item* 2)
+
+    else if(item === null)
+    console.log("Found a null item")
+
+    else 
+    {
+        result.push(parseInt(item))
     }
 });
 
-console.log("Original items:", items);
-console.log("Filtered and transformed items:", results);
-
-//CODE ABSOLUT
+console.log("Original items:", items)
+console.log("Filtered and transformed items:", results)
